c0d9aa431d2bcccc845f75918568a527737dab43
AnushkaD15 <storoid1508@gmail.com>
2023-03-02 16:53:53 +0530

    fig 4

---
+++ app/src/main/java/com/example/order_management_system/PartyDetailsActivity.java
@@ -0,0 +1,20 @@
+package com.example.order_management_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class PartyDetailsActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_party_details);
+
+
+    }
+
+
+}
