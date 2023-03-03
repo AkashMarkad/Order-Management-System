a4c5bbe22fec635dbce16ae7e8c40746f36a4475
AkashMarkad <akashmarkad2210@gmail.com>
2023-03-03 23:24:32 +0530

    Fig 1 updated

--- app/src/main/java/com/example/order_management_system/MainActivity.java
+++ app/src/main/java/com/example/order_management_system/MainActivity.java
@@ -3,14 +3,24 @@ package com.example.order_management_system;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button New , saveAndProceed;
+    ImageView moreOption;
+    CheckBox GroupProduct;
+    RadioGroup RGroup;
+    RadioButton selectedRadioButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +28,16 @@ public class MainActivity extends AppCompatActivity {
 
         New = findViewById(R.id.btnNew);
         saveAndProceed = findViewById(R.id.btnSaveAndProceed);
+        moreOption = findViewById(R.id.main_more);
+        GroupProduct = findViewById(R.id.checkBoxGroupProduct);
+        RGroup = findViewById(R.id.RGroup);
+
+        moreOption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"More Option clicked" , Toast.LENGTH_SHORT).show();
+            }
+        });
 
         New.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,5 +52,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this,ProductNameAndQRCode.class));
             }
         });
+
     }
 }
